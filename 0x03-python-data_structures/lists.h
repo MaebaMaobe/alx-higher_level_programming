571f5f4b8e85214ee99b465fff5a2c090c9fc9b7
MaebaMaobe <maebamaobe.com>
2022-06-06 18:06:38 +0000

    Prototype file

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * Data Structures
+ * @n: integer
+ *
+ * Description: Lists,Tuples
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+def print_list_integer(my_list=[]):
+def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
+def print_reversed_list_integer(my_list=[]):
+def new_in_list(my_list, idx, element):
+def no_c(my_string):
+
+#endif /* LISTS_H */
